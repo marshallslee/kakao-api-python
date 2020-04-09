bd42294cb58470d42dde90892851847a4acedf17
Marshall <marshall.s.lee@gmail.com>
2020-04-09 19:08:40 +0900

    under construction

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from geocode.geocode import get_latitude_and_longitude_by_address
 def run():
     address = input("Please enter a valid address.")
     latitude, longitude = get_latitude_and_longitude_by_address(address)
-    if latitude and longitude is not None:
+    if latitude and longitude:
         print("Latitude:", latitude)
         print("Longitude:", longitude)
     else:
