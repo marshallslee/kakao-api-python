6ab5cc5b8b8d0080dba114d4169519d0cc3470f9
Marshall <marshall@soomgo.com>
2020-04-09 19:07:02 +0900

    under construction

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from geocode.geocode import get_latitude_and_longitude_by_address
 def run():
     address = input("Please enter a valid address.")
     latitude, longitude = get_latitude_and_longitude_by_address(address)
-    print("Latitude:", latitude)
-    print("Longitude:", longitude)
+    if latitude and longitude is not None:
+        print("Latitude:", latitude)
+        print("Longitude:", longitude)
+    else:
+        print("Failed to calculate latitude and longitude from the given address.")
     return
 
 
--- geocode/geocode.py
+++ geocode/geocode.py
@@ -3,20 +3,19 @@ from config.config import api_key
 
 
 def get_latitude_and_longitude_by_address(address):
-    global latitude, longitude
+    latitude, longitude = None, None
     try:
         url = "https://dapi.kakao.com/v2/local/search/address.json"
 
         req_url = "{}?query={}".format(url, address);
         r = requests.get(req_url, headers={'Authorization': "KakaoAK {}".format(api_key)})
         data = r.json()
-        if len(data.get('documents', [])) is 0 or data.get('total_count', 0) is 0:
-            print('Request: {}, Response: {}'.format(req_url, data))
-            return
+
         latitude = data['documents'][0]['y']
         longitude = data['documents'][0]['x']
 
     except Exception as e:
         print(type(e))
         print(str(e))
-    return latitude, longitude
+    finally:
+        return latitude, longitude
